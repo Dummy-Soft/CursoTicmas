f2519db9d6104a0c98f5934fc587e72a90dafaf2
Carlos Alonso <tatico05@hotmail.com>
2023-02-27 07:44:54 -0300

    arrancamos con javascript

---
+++ Curso/JavaScript/tipo-de-variables.js
@@ -0,0 +1,8 @@
+
+
+let x;       // Definimos X
+console.log(x);
+x = 5;       // x le asignamos un valor numerico
+console.log(x);
+x = "John";  // x ahora almacena un string
+console.log(x);
---
+++ Curso/JavaScript/variables.js
@@ -0,0 +1,8 @@
+
+let longitud = 16;                          // Numero
+let apellido = "Johnson"                    // String
+let x = {nombre:"John", apellido:"Doe"};    // Objeto
+
+
+let y = 16 + "Volvo";
+console.log(y);
