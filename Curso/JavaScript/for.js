dc26f8cc186f2f8feae8d557e5301d5e0cf7c42e
Carlos Alonso <tatico05@hotmail.com>
2023-02-27 08:38:59 -0300

    Seguimos trabajando con JavaScript

---
+++ Curso/JavaScript/for.js
@@ -0,0 +1,7 @@
+
+// for (iniciarla; condicion; incrementa/decrementa)
+var x = 10;
+
+for (x; x > 0; x--) {
+    console.log("El valor de x es: "+x);
+}
---
+++ Curso/JavaScript/if.js
@@ -0,0 +1,8 @@
+
+var edad = 18
+
+if (edad > 17) {
+    console.log("Es mayor de edad")
+} else {
+    console.log("Es menor de edad")
+}
