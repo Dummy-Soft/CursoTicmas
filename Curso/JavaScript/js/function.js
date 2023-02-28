76399ce6143512fe40d949da2c667f18ac3a212a
Carlos Alonso <tatico05@hotmail.com>
2023-02-28 05:29:46 -0300

    eventos en javascript

--- Curso/JavaScript/js/function.js
+++ Curso/JavaScript/js/function.js
@@ -1,9 +1,10 @@
+//console.log("Hola Mundo!")
+//let numero1 = 3;
+//let numero2 = 4;
+//let resultado = numero1+numero2;
+//console.log("La Suma de los numeros es: "+resultado);
 
-console.log("Hola Mundo!")
-
-let numero1 = 3;
-let numero2 = 4;
-
-let resultado = numero1+numero2;
-
-console.log("La Suma de los numeros es: "+resultado);
+document.getElementById("boton").onclick = function() {
+    console.log("Leemos el clik al boton");
+    document.getElementById("demo").innerHTML = "Primer Evento en JavaScript";
+}
